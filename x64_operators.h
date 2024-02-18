abc2bcc2987ae7ca147459e07ab304c330bdd18a
Ne0nWinds <cadenparker@ymail.com>
2024-02-17 22:17:03 -0500

    X64 u32 types

--- x64_operators.h
+++ x64_operators.h
@@ -1,189 +1,214 @@
 /* == u32x2 == */
 MATHCALL u32x2 operator+(const u32x2 &a, const u32x2 &b) {
-
+	xmm Result = _mm_add_epi32(xmm(a), xmm(b));
+	return (u32x2)Result;
 }
 MATHCALL u32x2 operator-(const u32x2 &a, const u32x2 &b) {
-
+	xmm Result = _mm_sub_epi32(xmm(a), xmm(b));
+	return (u32x2)Result;
 }
 MATHCALL u32x2 operator*(const u32x2 &a, const u32x2 &b) {
-
+	xmm Result = _mm_mul_epi32(xmm(a), xmm(b));
+	return (u32x2)Result;
 }
+[[deprecated("SIMD Integer Division is unsupported on x64")]]
 MATHCALL u32x2 operator/(const u32x2 &a, const u32x2 &b) {
-
+	u32x2 Result = 0;
+	Result.x /= b.x;
+	Result.y /= b.y;
+	return Result;
 }
 MATHCALL u32x2 operator-(const u32x2 &a) {
-
+	u32x2 Result;
+	Result = ~a + 1;
+	return (u32x2)Result;
 }
 MATHCALL u32x2 operator==(const u32x2 &a, const u32x2 &b) {
-
+	xmm Result = _mm_cmpeq_epi32(xmm(a), xmm(b));
+	return (u32x2)Result;
 }
 MATHCALL u32x2 operator!=(const u32x2 &a, const u32x2 &b) {
-
+	xmm Equal = _mm_cmpeq_epi32(xmm(a), xmm(b));
+	u32x2 Result = (u32x2)Equal;
+	Result = ~Result;
+	return Result;
 }
 MATHCALL u32x2 operator>(const u32x2 &a, const u32x2 &b) {
-
+	xmm Result = _mm_cmpgt_epi32(xmm(a), xmm(b));
+	return (u32x2)Result;
 }
 MATHCALL u32x2 operator<(const u32x2 &a, const u32x2 &b) {
-
+	xmm Result = _mm_cmplt_epi32(xmm(a), xmm(b));
+	return (u32x2)Result;
 }
 MATHCALL u32x2 operator&(const u32x2 &a, const u32x2 &b) {
-
+	xmm Result = _mm_and_si128(xmm(a), xmm(b));
+	return (u32x2)Result;
 }
 MATHCALL u32x2 operator|(const u32x2 &a, const u32x2 &b) {
-
+	xmm Result = _mm_or_si128(xmm(a), xmm(b));
+	return (u32x2)Result;
 }
 MATHCALL u32x2 operator^(const u32x2 &a, const u32x2 &b) {
-
+	xmm Result = _mm_xor_si128(xmm(a), xmm(b));
+	return (u32x2)Result;
 }
 MATHCALL u32x2 operator~(const u32x2 &a) {
-
+	xmm Throwaway = xmm((u32)0);
+	xmm AllOnes = _mm_cmpeq_epi32(Throwaway, Throwaway);
+	xmm Result = _mm_xor_ps(AllOnes, xmm(a));
+	return (u32x2)Result;
 }
 MATHCALL u32x2 operator<<(const u32x2 &a, const u32 &b) {
-
+	xmm Result = _mm_slli_epi32(xmm(a), b);
+	return (u32x2)Result;
 }
 MATHCALL u32x2 operator>>(const u32x2 &a, const u32 &b) {
-
+	xmm Result = _mm_srli_epi32(xmm(a), b);
+	return (u32x2)Result;
 }
 
 /* == u32x3 ==*/
 MATHCALL u32x3 operator+(const u32x3 &a, const u32x3 &b) {
-
+	xmm Result = _mm_add_epi32(xmm(a), xmm(b));
+	return (u32x3)Result;
 }
 MATHCALL u32x3 operator-(const u32x3 &a, const u32x3 &b) {
-
+	xmm Result = _mm_sub_epi32(xmm(a), xmm(b));
+	return (u32x3)Result;
 }
 MATHCALL u32x3 operator*(const u32x3 &a, const u32x3 &b) {
-
+	xmm Result = _mm_mul_epi32(xmm(a), xmm(b));
+	return (u32x3)Result;
 }
+[[deprecated("SIMD Integer Division is unsupported on x64")]]
 MATHCALL u32x3 operator/(const u32x3 &a, const u32x3 &b) {
-
+	u32x3 Result = 0;
+	Result.x /= b.x;
+	Result.y /= b.y;
+	Result.z /= b.z;
+	return Result;
 }
 MATHCALL u32x3 operator-(const u32x3 &a) {
-
+	u32x3 Result;
+	Result = ~a + 1;
+	return (u32x3)Result;
 }
 MATHCALL u32x3 operator==(const u32x3 &a, const u32x3 &b) {
-
+	xmm Result = _mm_cmpeq_epi32(xmm(a), xmm(b));
+	return (u32x3)Result;
 }
 MATHCALL u32x3 operator!=(const u32x3 &a, const u32x3 &b) {
-
+	xmm Equal = _mm_cmpeq_epi32(xmm(a), xmm(b));
+	u32x3 Result = (u32x3)Equal;
+	Result = ~Result;
+	return Result;
 }
 MATHCALL u32x3 operator>(const u32x3 &a, const u32x3 &b) {
-
+	xmm Result = _mm_cmpgt_epi32(xmm(a), xmm(b));
+	return (u32x3)Result;
 }
 MATHCALL u32x3 operator<(const u32x3 &a, const u32x3 &b) {
-
+	xmm Result = _mm_cmplt_epi32(xmm(a), xmm(b));
+	return (u32x3)Result;
 }
 MATHCALL u32x3 operator&(const u32x3 &a, const u32x3 &b) {
-
+	xmm Result = _mm_and_si128(xmm(a), xmm(b));
+	return (u32x3)Result;
 }
 MATHCALL u32x3 operator|(const u32x3 &a, const u32x3 &b) {
-
+	xmm Result = _mm_or_si128(xmm(a), xmm(b));
+	return (u32x3)Result;
 }
 MATHCALL u32x3 operator^(const u32x3 &a, const u32x3 &b) {
-
+	xmm Result = _mm_xor_si128(xmm(a), xmm(b));
+	return (u32x3)Result;
 }
 MATHCALL u32x3 operator~(const u32x3 &a) {
-
+	xmm Throwaway = xmm((u32)0);
+	xmm AllOnes = _mm_cmpeq_epi32(Throwaway, Throwaway);
+	xmm Result = _mm_xor_ps(AllOnes, xmm(a));
+	return (u32x3)Result;
 }
 MATHCALL u32x3 operator<<(const u32x3 &a, const u32 &b) {
-
+	xmm Result = _mm_slli_epi32(xmm(a), b);
+	return (u32x3)Result;
 }
 MATHCALL u32x3 operator>>(const u32x3 &a, const u32 &b) {
-
+	xmm Result = _mm_srli_epi32(xmm(a), b);
+	return (u32x3)Result;
 }
 
-/* == u32x4 == */
+/* == u32x4 ==*/
 MATHCALL u32x4 operator+(const u32x4 &a, const u32x4 &b) {
-
+	xmm Result = _mm_add_epi32(xmm(a), xmm(b));
+	return (u32x4)Result;
 }
 MATHCALL u32x4 operator-(const u32x4 &a, const u32x4 &b) {
-
+	xmm Result = _mm_sub_epi32(xmm(a), xmm(b));
+	return (u32x4)Result;
 }
 MATHCALL u32x4 operator*(const u32x4 &a, const u32x4 &b) {
-
+	xmm Result = _mm_mul_epi32(xmm(a), xmm(b));
+	return (u32x4)Result;
 }
+[[deprecated("SIMD Integer Division is unsupported on x64")]]
 MATHCALL u32x4 operator/(const u32x4 &a, const u32x4 &b) {
-
+	u32x4 Result = 0;
+	Result.x /= b.x;
+	Result.y /= b.y;
+	Result.z /= b.z;
+	Result.w /= b.w;
+	return Result;
 }
 MATHCALL u32x4 operator-(const u32x4 &a) {
-
+	u32x4 Result;
+	Result = ~a + 1;
+	return (u32x4)Result;
 }
 MATHCALL u32x4 operator==(const u32x4 &a, const u32x4 &b) {
-
+	xmm Result = _mm_cmpeq_epi32(xmm(a), xmm(b));
+	return (u32x4)Result;
 }
 MATHCALL u32x4 operator!=(const u32x4 &a, const u32x4 &b) {
-
+	xmm Equal = _mm_cmpeq_epi32(xmm(a), xmm(b));
+	u32x4 Result = (u32x4)Equal;
+	Result = ~Result;
+	return Result;
 }
 MATHCALL u32x4 operator>(const u32x4 &a, const u32x4 &b) {
-
+	xmm Result = _mm_cmpgt_epi32(xmm(a), xmm(b));
+	return (u32x4)Result;
 }
 MATHCALL u32x4 operator<(const u32x4 &a, const u32x4 &b) {
-
+	xmm Result = _mm_cmplt_epi32(xmm(a), xmm(b));
+	return (u32x4)Result;
 }
 MATHCALL u32x4 operator&(const u32x4 &a, const u32x4 &b) {
-
+	xmm Result = _mm_and_si128(xmm(a), xmm(b));
+	return (u32x4)Result;
 }
 MATHCALL u32x4 operator|(const u32x4 &a, const u32x4 &b) {
-
+	xmm Result = _mm_or_si128(xmm(a), xmm(b));
+	return (u32x4)Result;
 }
 MATHCALL u32x4 operator^(const u32x4 &a, const u32x4 &b) {
-
+	xmm Result = _mm_xor_si128(xmm(a), xmm(b));
+	return (u32x4)Result;
 }
 MATHCALL u32x4 operator~(const u32x4 &a) {
-
+	xmm Throwaway = xmm((u32)0);
+	xmm AllOnes = _mm_cmpeq_epi32(Throwaway, Throwaway);
+	xmm Result = _mm_xor_ps(AllOnes, xmm(a));
+	return (u32x4)Result;
 }
 MATHCALL u32x4 operator<<(const u32x4 &a, const u32 &b) {
-
+	xmm Result = _mm_slli_epi32(xmm(a), b);
+	return (u32x4)Result;
 }
 MATHCALL u32x4 operator>>(const u32x4 &a, const u32 &b) {
-
-}
-
-/* == u32x8 == */
-MATHCALL u32x8 operator+(const u32x8 &a, const u32x8 &b) {
-
-}
-MATHCALL u32x8 operator-(const u32x8 &a, const u32x8 &b) {
-
-}
-MATHCALL u32x8 operator*(const u32x8 &a, const u32x8 &b) {
-
-}
-MATHCALL u32x8 operator/(const u32x8 &a, const u32x8 &b) {
-
-}
-MATHCALL u32x8 operator-(const u32x8 &a) {
-
-}
-MATHCALL u32x8 operator==(const u32x8 &a, const u32x8 &b) {
-
-}
-MATHCALL u32x8 operator!=(const u32x8 &a, const u32x8 &b) {
-
-}
-MATHCALL u32x8 operator>(const u32x8 &a, const u32x8 &b) {
-
-}
-MATHCALL u32x8 operator<(const u32x8 &a, const u32x8 &b) {
-
-}
-MATHCALL u32x8 operator&(const u32x8 &a, const u32x8 &b) {
-
-}
-MATHCALL u32x8 operator|(const u32x8 &a, const u32x8 &b) {
-
-}
-MATHCALL u32x8 operator^(const u32x8 &a, const u32x8 &b) {
-
-}
-MATHCALL u32x8 operator~(const u32x8 &a) {
-
-}
-MATHCALL u32x8 operator<<(const u32x8 &a, const u32 &b) {
-
-}
-MATHCALL u32x8 operator>>(const u32x8 &a, const u32 &b) {
-
+	xmm Result = _mm_srli_epi32(xmm(a), b);
+	return (u32x4)Result;
 }
 
 /* == v2 == */
@@ -381,7 +406,7 @@ MATHCALL v4 operator>>(const v4 &a, const u32 &b) {
 	return (v4)Result;
 }
 
-/* == v8 == */
+/* == v8 / u32x8 == */
 #if SIMD_WIDTH == 4
 MATHCALL v8 operator+(const v8 &a, const v8 &b) {
 	xmm xmm0 = _mm_add_ps(xmm(a.V4[0]), xmm(b.V4[0]));
